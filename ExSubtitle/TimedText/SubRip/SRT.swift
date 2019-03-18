d9d88a8bf847b7dc24afacc7da76a3b233283f12
Seungho Hong <shhong@inisoft.co.kr>
2019-03-18 15:47:55 +0900

    replace default api sources

---
+++ ExSubtitle/TimedText/SubRip/SRT.swift
@@ -0,0 +1,25 @@
+
+/*
+ * https://en.wikipedia.org/wiki/SubRip 
+ * application/x-subrip
+
+ 1
+ 00:00:13,800 --> 00:00:16,150
+ No, that's not how it is.
+
+ 2
+ 00:00:16,150 --> 00:00:17,380
+ I didn't mean that.
+
+ */
+
+import Foundation
+
+class SRT: TimedText {
+    var cues: [Cue] = []
+
+    func parse(_ data: Data, completion: @escaping (Bool, Error?) -> Void) {
+        // TODO: line parser
+        completion(true, nil)
+    }
+}
